08831519a875d031a3424daa419649e0d8897980
Your Name <solomondin@gmail.com>
2023-11-28 13:14:54 -0500

    javascript array looping

--- CrashCourse/Project/script.js
+++ CrashCourse/Project/script.js
@@ -12,15 +12,6 @@ btn.addEventListener("click", function () {
 });
 
 /*
-function calcFactAge(year) {
-  const currentYear = new Date().getFullYear();
-  const age = currentYear - year;
-
-  if (age >= 0) return age;
-  else
-    return `Impossible Year. Year must be less or equal to ${currentYear} year.`;
-}
-
 const age1 = calcFactAge(2015);
 console.log(age1);
 console.log(calcFactAge(2020));
@@ -66,20 +57,26 @@ console.log(createHelloWorld());
 /*
 */
 
-/*
 const fact = ["Lisbon is the capital of Portugal", 2015, true];
-console.log(fact[0]);
 console.log(fact[1]);
 console.log(fact.length);
 console.log(fact[fact.length - 1]);
 
-//const [text, createdIn, isCorrect] = fact;
-//console.log(createdIn);
-
+const [text, createdIn, isCorrect] = fact;
 const newFact = [...fact, "society"];
 console.log(newFact);
 
-*/
+// [2, 4, 6, 8].forEach(function (el) {
+//   console.log(el);
+// });
+
+// const times2 = [2, 4, 6, 8].map(function (el) {
+//   return el * 2;
+// });
+
+const times2 = [2, 4, 6, 8].map((el) => el * 2);
+
+console.log(times2);
 
 /*
 const factObj = {
@@ -95,6 +92,7 @@ const { text, createdIn, isCorrect } = factObj;
 console.log(factObj.createSummary());
 */
 
+/*
 const factObj = {
   text: "Lisbon is the capital of Portugal",
   createdIn: 2015,
@@ -108,8 +106,74 @@ const factObj = {
   },
 };
 
+
 // Destructuring assignment to extract properties from the object
 const { text, createdIn, isCorrect } = factObj;
 
 // Accessing the 'createSummary' method from the object
 console.log(factObj.createSummary());
+
+*/
+
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+const allCategories = CATEGORIES.map((category) => category.name);
+
+console.log(allCategories);
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2045,
+  },
+];
+
+function calcFactAge(year) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - year;
+
+  if (age >= 0) return age;
+  else
+    return `Impossible Year. Year must be less or equal to ${currentYear} year.`;
+}
+
+const factAges = initialFacts.map((fact) => calcFactAge(fact.createdIn));
+
+console.log(factAges);
+console.log(factAges.join(", "));
