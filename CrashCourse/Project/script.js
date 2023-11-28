1148507b8117eea71bd85f70aeb6adf4c1f33f38
Your Name <solomondin@gmail.com>
2023-11-28 10:12:28 -0500

    javascript objects

--- CrashCourse/Project/script.js
+++ CrashCourse/Project/script.js
@@ -11,6 +11,7 @@ btn.addEventListener("click", function () {
   }
 });
 
+/*
 function calcFactAge(year) {
   const currentYear = new Date().getFullYear();
   const age = currentYear - year;
@@ -25,8 +26,12 @@ console.log(age1);
 console.log(calcFactAge(2020));
 console.log(calcFactAge(2037));
 
-const calcFactAge2 = (year) => new Date().getFullYear() - year;
+const calcFactAge2 = (year) =>
+  year <= new Date().getFullYear()
+    ? new Date().getFullYear() - year
+    : `Impossible Year. Year must be less or equal to ${new Date().getFullYear()} year.`;
 console.log(calcFactAge2(2015));
+console.log(calcFactAge2(2037));
 
 let votesInteresting = 20;
 let votesMindblowing = 5;
@@ -58,3 +63,53 @@ console.log(str);
 let createHelloWorld = () => "Hello World";
 
 console.log(createHelloWorld());
+/*
+*/
+
+/*
+const fact = ["Lisbon is the capital of Portugal", 2015, true];
+console.log(fact[0]);
+console.log(fact[1]);
+console.log(fact.length);
+console.log(fact[fact.length - 1]);
+
+//const [text, createdIn, isCorrect] = fact;
+//console.log(createdIn);
+
+const newFact = [...fact, "society"];
+console.log(newFact);
+
+*/
+
+/*
+const factObj = {
+  text: "Lisbon is the capital of Portugal",
+  createdIn: 2015,
+  isCorrect: true,
+  createSummary: function () {
+    return`The fact is ${this.text}. It is about ${this.createdIn} years old. It is probably ${this.is`
+};
+
+const { text, createdIn, isCorrect } = factObj;
+
+console.log(factObj.createSummary());
+*/
+
+const factObj = {
+  text: "Lisbon is the capital of Portugal",
+  createdIn: 2015,
+  isCorrect: true,
+  createSummary: function () {
+    return `The fact is ${this.text}. It is about ${
+      new Date().getFullYear() - this.createdIn
+    } years old. It is probably ${
+      this.isCorrect ? "correct" : "incorrect"
+    } fact.`;
+  },
+};
+
+// Destructuring assignment to extract properties from the object
+const { text, createdIn, isCorrect } = factObj;
+
+// Accessing the 'createSummary' method from the object
+console.log(factObj.createSummary());
