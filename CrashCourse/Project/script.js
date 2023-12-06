a7b5c84cd73f075f71c3bdf375a606a44d1b9f7f
Your Name <solomondin@gmail.com>
2023-12-06 09:40:59 -0500

    javascript practice

--- CrashCourse/Project/script.js
+++ CrashCourse/Project/script.js
@@ -1,6 +1,79 @@
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2045,
+  },
+];
+
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+//Selecting DOM elements
 const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
+const factsList = document.querySelector(".facts-list");
 
+// Create DOM elements: Render facts in list
+factsList.innerHTML = "";
+createFactList(initialFacts);
+
+function createFactList(dataArray) {
+  const htmlArr = dataArray.map(
+    (fact) => `<li class="fact"> 
+  <p>
+  ${fact.text}
+  <a
+    class="source"
+    href="${fact.source}"
+    target="_blank"
+    >(Source)</a
+  >
+  </p>
+  <span class="tag" style="background-color:
+  #3b82f6">${fact.category}</span>
+ </li>`
+  );
+
+  const html = htmlArr.join("");
+  factsList.insertAdjacentHTML("afterbegin", html);
+}
+
+// Toggle form visibility
 btn.addEventListener("click", function () {
   if (form.classList.contains("hidden")) {
     form.classList.remove("hidden");
@@ -11,6 +84,7 @@ btn.addEventListener("click", function () {
   }
 });
 
+console.dir(btn);
 /*
 const age1 = calcFactAge(2015);
 console.log(age1);
@@ -54,9 +128,10 @@ console.log(str);
 let createHelloWorld = () => "Hello World";
 
 console.log(createHelloWorld());
-/*
-*/
 
+const age1 = calcFactAge(2015);
+console.log(age1);
+console.log(calcFactAge(2020));
 const fact = ["Lisbon is the capital of Portugal", 2015, true];
 console.log(fact[1]);
 console.log(fact.length);
@@ -78,7 +153,7 @@ const times2 = [2, 4, 6, 8].map((el) => el * 2);
 
 console.log(times2);
 
-/*
+
 const factObj = {
   text: "Lisbon is the capital of Portugal",
   createdIn: 2015,
@@ -90,9 +165,9 @@ const factObj = {
 const { text, createdIn, isCorrect } = factObj;
 
 console.log(factObj.createSummary());
-*/
 
-/*
+
+
 const factObj = {
   text: "Lisbon is the capital of Portugal",
   createdIn: 2015,
@@ -113,8 +188,12 @@ const { text, createdIn, isCorrect } = factObj;
 // Accessing the 'createSummary' method from the object
 console.log(factObj.createSummary());
 
-*/
 
+
+
+const factObj = {
+  text: "Lisbon is the capital of Portugal",
+  createdIn: 2015,
 const CATEGORIES = [
   { name: "technology", color: "#3b82f6" },
   { name: "science", color: "#16a34a" },
@@ -177,3 +256,4 @@ const factAges = initialFacts.map((fact) => calcFactAge(fact.createdIn));
 
 console.log(factAges);
 console.log(factAges.join(", "));
+*/
